d7ca9ff30ac63a7bbb49f7cc33f921ff8f213b24
kulcidi <kullciid252@gmail.com>
2024-10-03 09:44:31 +0900

    footer work in process

--- app/page.js
+++ app/page.js
@@ -69,7 +69,6 @@ export default function Home() {
 				</div>
 			</header>
 
-
 			<aside
 				className={`w-9/12 h-screen bg-white shadow-custom fixed top-98px left-0 z-40 transform transition-transform duration-300 ${
 					isAsideOpen ? 'translate-x-0' : '-translate-x-full'
@@ -92,16 +91,72 @@ export default function Home() {
 						<input
 							className='inter-font bg-[#ffffff0f] focus:outline-none'
 							type='text'
-							placeholder='Search Art Work / Creator'
+							placeholder='Search'
 						/>
 					</div>
 					<button className='bg-black text-white py-4 px-7 h-auto rounded-2xl'>
-						<h4 className='popa-font ml-1 text-2xl uppercase'>
+						<h4 className='popa-font ml-1 text-1xl uppercase'>
 							Connect Wallet
 						</h4>
 					</button>
 				</div>
 			</aside>
+
+			<footer className='py-6 visible w-full bg-black text-white'>
+				<div className='flex px-6'>
+					<div className='flex w-full justify-between items-start'>
+						<button className='flex items-center'>
+							<Image
+								src='/Logo.svg'
+								alt='Logo'
+								className='logo'
+								width={50}
+								height={50}
+							/>
+							<b className='popa-font ml-1 text-3xl'>DiveSea</b>
+						</button>
+						<div className='flex gap-4 mt-4 items-end'>
+							<div className='hover:fill-white'>
+								<Image src='/inst.svg' alt='Instagram' width={24} height={24} />
+							</div>
+							<div className='hover:fill-white'>
+								<Image src='/inbook.svg' alt='Inbook' width={24} height={24} />
+							</div>
+							<div className='hover:fill-white'>
+								<Image
+									src='/facebook.svg'
+									alt='Facebook'
+									width={12}
+									height={12}
+								/>
+							</div>
+							<div className='hover:fill-white'>
+								<Image
+									src='/twitter.svg'
+									alt='Twitter'
+									width={24}
+									height={24}
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className='ml-5 flex flex-col gap-5 pt-8'>
+					<b className='inter-font font-thin ml-1 text-2xl uppercase'>
+						Privacy Policy
+					</b>
+					<b className='inter-font font-thin ml-1 text-2xl uppercase'>
+						term & Conditions
+					</b>
+					<b className='inter-font font-thin ml-1 text-2xl uppercase'>
+						About sUs
+					</b>
+					<b className='inter-font font-thin ml-1 text-2xl uppercase'>
+						Contact
+					</b>
+				</div>
+				<div className='border-solid border-b border-white ml-4 mr-4'></div>
+			</footer>
 		</div>
 	)
 }
